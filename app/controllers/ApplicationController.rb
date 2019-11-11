16f591700fa893253ade3e21ea4f19142775ce4b
Marzouk Mariem <36210586+marzoukmariem@users.noreply.github.com>
2019-11-11 20:57:04 +0100

    Initial commit

---
+++ app/controllers/ApplicationController.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+class ApplicationController < Sinatra::Base
+	# This configuration part will inform the app where to search for the views and from where it will serve the static files
+  	configure do
+    	set :views, "app/views"
+    	set :public_dir, "public"
+  	end
+
+  	get '/' do
+   		erb :index
+  	end
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+require './app/controllers/ApplicationController'
+#require_relative './config/environment'
+
+run ApplicationController
